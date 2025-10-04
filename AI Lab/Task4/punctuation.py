f4bfa124283da7ad234c08b2a936ba41c784f915
JibraanZaidi <jibraanali@gmail.com>
2025-10-04 16:15:02 +0500

    Delete AI Lab/Task4 directory

--- AI Lab/Task4/punctuation.py
+++
@@ -1,8 +0,0 @@
-punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-
-def remove_punctuation(text):
-    return ''.join(char for char in text if char not in punctuations)
-
-sample = "Hello, Jibraan Zaidi kas'a ho! bha'i? kia kry/// ho?"
-result = remove_punctuation(sample)
-print(result)
